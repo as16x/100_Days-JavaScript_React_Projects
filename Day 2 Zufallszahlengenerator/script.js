d7fef1bb59f55d4c9e6b6c1ed41a5639b2748373
thecodedoctor <93331983+as16x@users.noreply.github.com>
2022-05-23 20:21:15 +0200

    Update script.js

--- Day 2 Zufallszahlengenerator/script.js
+++ Day 2 Zufallszahlengenerator/script.js
@@ -8,6 +8,6 @@ function generateNumber() {
     //Math.random ist eine Methode um einen Zufallswert zwischen 0 und 1 zu generieren. (z.B. 0.83746, 0.83847, 0.56233 ...)
     //Wenn wir jetzt mit 1000 multiplizieren, erhalten wir einen Wert zwischen 0 to 999.99999. Um die 1000 einzuschließen, müssen wir am Ende noch eine 1 ergänzen.
     const rand = Math.floor(Math.random() * 1000  + 1);
-    //Da uns Math.random viele Dezimalzahlen erzeugt, verwenden wir Math.floor, um aus einer Dezimalzahl eine ganze Zahl zu erhalten.
+    //Da uns die Funktion Math.random nur Dezimalzahlen erzeugt, verwenden wir ergänzend Math.floor, um aus einer Dezimalzahl eine ganze Zahl zu erhalten.
     number.innerHTML = rand;
 }
